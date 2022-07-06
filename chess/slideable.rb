2adbcbc8d4d25e948429661cf2af67662d65c818
Truong Nguyen <trnguyen34@gmail.com>
2022-07-06 09:04:51 -0700

    added a few moves

--- chess/slideable.rb
+++ chess/slideable.rb
@@ -1,4 +1,16 @@
 module Slideable
-    HORIZONTAL_DIRS = [[0, 1], [1, 0]]
-    DIAGONAL_DIRS = [[1, 1], [1, -1]]
+    HORIZONTAL_DIRS = [[0, 1], [1, 0], [-1, 0], [0, -1]]
+    DIAGONAL_DIRS = [[1, 1], [1, -1], [-1, -1], [-1, 1]]
+end
+
+def horizontal_dir
+    HORIZONTAL_DIRS
+end
+
+def diagonal_dir
+    DIAGONAL_DIRS
+end
+
+def moves
+    
 end
