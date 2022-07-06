832f0ae89cbba167dad687b86d877ac68ee5b44c
Truong Nguyen <trnguyen34@gmail.com>
2022-07-06 14:58:55 -0700

    finsish with phase 2

--- chess/game.rb
+++ chess/game.rb
@@ -1,7 +1,7 @@
 require_relative "display.rb"
 require 'byebug'
 class Game
-    attr_reader :display
+    attr_reader :display, :board
     def initialize
         @board = Board.new
         @display = Display.new(@board)
--- chess/slideable.rb
+++ chess/slideable.rb
@@ -12,61 +12,26 @@ module Slideable
     end
 
     def moves
-=begin
-
-        Iterate through move_dirs
-
-        inject starting wiht empty array, pass each vector from move_dir into grow_un...
-        and add it to the empty array
-
-
-=end
-        move_dirs.inject([]) {|acc, curr| acc + grow_unblocked_moves_in_dir(*curr)}
-        
+        move_dirs.inject([]) {|acc, curr| acc + grow_unblocked_moves_in_dir(*curr)} 
     end
 
+    private
     def move_dirs
         []
-
     end
 
     def grow_unblocked_moves_in_dir(dx, dy)
-        # debugger
-
         step = [dx, dy]
-
         moves = []
-
         currpos = addpos(pos, step)
 
         while(Board.on_board?(currpos))
-        
             return moves if(board[currpos].color == color)
-
             moves << currpos
-
             return moves if board[currpos].color == enemy
-
             currpos = addpos(currpos, step)
-
         end
 
         return moves
-=begin
-Start with curren position and iterate by adding dx, dy to position
-        Stop if you move out of the board
-
-        If you hit a piece:
-            If it's an enemy, add this last position
-            Break
-        Else if it's a friendly one
-            Don´t add this last position
-            Break
-        Else
-            Add the position
-        End
-
-=end
-
     end
 end
